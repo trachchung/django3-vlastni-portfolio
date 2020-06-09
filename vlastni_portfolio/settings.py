12ee0fb12bd93b902c9a821cca194b3a52d9bce8
trachchung <nguyentrachchung@gmail.com>
2020-06-09 14:38:25 +0000

    Remove old stuff

--- vlastni_portfolio/settings.py
+++ vlastni_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n5*)*#f9+^=yq+aggv7e6!psmj7vr0b%_cm#b5!3j^y^h2xmn+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trachchung.pythonanywhere.com']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
